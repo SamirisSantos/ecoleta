0d4f229247973ffc780b918be60c8925f4515fb4
Samiris Santos <samiris.araujo@gmail.com>
2020-06-05 15:18:48 -0300

    install nodemon

--- src/server.js
+++ src/server.js
@@ -1,5 +1,10 @@
 const express = require ("express")
 const server = express()
 
+//pagina inicial
+server.get("/", (req,res)=>{
+    res.send("Servidor rodando")
+})
+
 //iniciar o servidor
 server.listen(3000)
