d6a168eaf6ef989c9aedec589e7cc890db266d76
Samiris Santos <samiris.araujo@gmail.com>
2020-06-05 15:32:02 -0300

    primeiras rodas get

--- src/server.js
+++ src/server.js
@@ -1,9 +1,20 @@
 const express = require ("express")
 const server = express()
 
+//configurar pasta publica
+server.use(express.static("public"))
+
 //pagina inicial
 server.get("/", (req,res)=>{
-    res.send("Servidor rodando")
+    res.sendFile(__dirname + "/views/index.html")
+})
+
+server.get("/create-point", (req,res)=>{
+    res.sendFile(__dirname + "/views/create-point.html")
+})
+
+server.get("/search-results", (req,res)=>{
+    res.sendFile(__dirname + "/views/search-results.html")
 })
 
 //iniciar o servidor
